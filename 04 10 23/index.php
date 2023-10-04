73f896b47d1e2725dabf3fa0993dcee5e70a001a
AndreCrimsom <andre.faquineti@etec.sp.gov.br>
2023-10-04 20:30:20 -0300

    quase

--- 04 10 23/index.php
+++ 04 10 23/index.php
@@ -1,1 +1,23 @@
-aaaaa
+<!DOCTYPE html>
+<html lang="pt-br">
+    <header>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-witdh, initial-scale=1">
+        <title>Calculadora PHP</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </header>
+    <body>
+        <div class="input-group">
+            <span class="input-group-text">First and last name</span>
+            <input type="text" aria-label="First name" class="form-control">
+            <input type="text" aria-label="Last name" class="form-control">
+          </div>
+
+        <!--Bootstrap-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+</html>
+<!--
+Fontes
+https://getbootstrap.com/docs/5.3/forms/input-group/#multiple-inputs
+-->
